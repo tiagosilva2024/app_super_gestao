3870e497399fab46c8bf6acc5bb350d9cac8b1bb
tiagosilva2024 <tiago.silva@paranacidade.org.br>
2024-02-07 16:15:25 -0300

    commit inicial

---
+++ resources/views/site/principal.blade.php
@@ -0,0 +1,12 @@
+<h3>Principal (view)</h3>
+<!--<ul>
+    <li><a href="/">Principal</a></li>
+    <li><a href="/sobre">Sobre</a></li>
+    <li><a href="/contato">Contato</a></li>
+</ul>-->
+
+<ul>
+    <li><a href="{{ route('site.index') }}">Principal</a></li>
+    <li><a href="{{ route('site.sobre') }}">Sobre</a></li>
+    <li><a href="{{ route('site.contato') }}">Contato</a></li>
+</ul>
---
+++ routes/web.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+/*Route::get('/', function () {
+    return 'Hello World!';
+});
+*/
+
+Route::get ('/', [\App\Http\Controllers\PrincipalController::class, 'principal'])->name('site.index');
+Route::get ('/sobre', [\App\Http\Controllers\SobreController::class, 'sobre'])->name('site.sobre');
+Route::get ('/contato', [\App\Http\Controllers\ContatoController::class, 'contato'])->name('site.contato');
+Route::get ('/login', function(){ return 'Login';})->name('site.login');
+
+
+Route::prefix('/app')->group(function() {
+    Route::get ('/clientes', function(){ return 'Clientes';})->name('app.clientes');
+    Route::get ('/fornecedores', function(){ return 'Fornecedores';})->name('app.fornecedores');
+    Route::get ('/produtos', function(){ return 'Produtos';})->name('app.produtos');
+});
+
+
+Route::get('/teste/{p1}/{p2}', [\App\Http\Controllers\TesteController::class, 'teste'])->name('teste');
+
+Route::fallback(function() {
+    echo 'A rota acessada não existe. <a href="'.route('site.index').'">Clique aqui</a> para acessar à página inicial';
+});
+
+
