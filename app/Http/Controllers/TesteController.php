d1ac7487802636292212d34d765058809f7e04b4
tiagosilva2024 <tiago.silva@paranacidade.org.br>
2024-02-07 16:56:12 -0300

    testando o with()

--- app/Http/Controllers/TesteController.php
+++ app/Http/Controllers/TesteController.php
@@ -13,6 +13,6 @@ class TesteController extends Controller
         
         //return view('site.teste', compact('p1', 'p2')); //compact
 
-        return view('site.teste')->with('p1', $p1)  //with()
+        return view('site.teste')->with('p1', $p1)->with('p2', $p2);  //with
     }
 }
