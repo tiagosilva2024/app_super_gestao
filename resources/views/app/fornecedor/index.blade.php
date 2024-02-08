d9bfca696c1b4ef9b04af3afc05e4fd56e9c0569
tiagosilva2024 <tiago.silva@paranacidade.org.br>
2024-02-08 09:50:27 -0300

    Incluindo blocos PHP dentro do blade

---
+++ resources/views/app/fornecedor/index.blade.php
@@ -0,0 +1,10 @@
+<h3>Fornecedor</h3>
+
+{{-- Aqui fica o comentário que será descartado pelo interpretador do blade --}}
+
+{{ 'Teste 1, ' }}
+<?= 'Teste 2 e '?>
+@php
+    // Aqui podem ser utilizado comentários nativos do PHP
+    echo 'Teste 3';
+@endphp
